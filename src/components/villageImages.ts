b9f829bc95703ede18de4b703beb825a948e459f
alexischry123x <alexischry@gmail.com>
2025-09-01 15:52:36 +0300

    Create villageImages.ts
    created the villageImages for try.

---
+++ src/components/villageImages.ts
@@ -0,0 +1,8 @@
+// src/components/villageImages.ts
+export const villageImages: Record<string, string[]> = {
+  '1': ['/images/lefkara1.jpg', '/images/lefkara2.jpg', '/images/lefkara3.jpg'],
+  '2': ['/images/omodos1.jpg', '/images/omodos2.jpg'],
+  '3': ['/images/kakopetria1.jpg', '/images/kakopetria2.jpg', '/images/kakopetria3.jpg'],
+  '4': ['/images/platres1.jpg', '/images/platres2.jpg'],
+  '5': ['/images/lania1.jpg', '/images/lania2.jpg', '/images/lania3.jpg'],
+};
