fcf39cd69d233323947c628175baaa01c4c3b12d
alexischry123x <alexischry@gmail.com>
2025-09-02 13:06:24 +0300

    Update main.tsx
    add cart everytwhere

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App.tsx';
+import { CartProvider } from './components/CartContext';
+
+import './index.css';
 
 // Remove dark mode class addition
-createRoot(document.getElementById("root")!).render(<App />);
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </React.StrictMode>
+);
