2881431d4d559dcdb831e49f6a688d761c7feba0
alexischry123x <alexischry@gmail.com>
2025-09-02 13:06:58 +0300

    Create Basket.tsx
    added new Basket.tsx

---
+++ src/components/Basket.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useCart } from './CartContext';
+import { Link } from 'react-router-dom';
+
+const Basket: React.FC = () => {
+  const { cart } = useCart();
+
+  const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+
+  return (
+    <Link
+      to="/products"
+      className="fixed top-4 right-4 bg-white shadow-lg rounded-lg px-4 py-2 flex items-center space-x-2 cursor-pointer z-50"
+      title="View Basket"
+    >
+      <span role="img" aria-label="basket" className="text-xl">
+        🛒
+      </span>
+      <span className="font-semibold">
+        {totalItems} item{totalItems !== 1 ? 's' : ''}
+      </span>
+    </Link>
+  );
+};
+
+export default Basket;
