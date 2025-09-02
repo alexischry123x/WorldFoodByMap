400797518f0f4b69fb9078bb6faedf76b98585d6
alexischry123x <alexischry@gmail.com>
2025-09-02 14:31:55 +0300

    Update main.tsx
    attempt to fix cart

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App.tsx';
-import { CartProvider } from './components/CartContext';
-
 import './index.css';
-
-// Remove dark mode class addition
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <App />
   </React.StrictMode>
 );
